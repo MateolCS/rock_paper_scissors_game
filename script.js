4c1eae0e617c2aa9b49dd1dbdb8fb8533074ed1b
MateoLCS <mateusz.namysl0318@gmail.com>
2021-11-24 11:53:32 +0100

    Final form

--- script.js
+++ script.js
@@ -6,20 +6,28 @@ let playerPickScissors = document.querySelector('#player-pick-scissors')
 
 let displayResult = document.querySelector('#display-result > h2')
 
+let displayPlayerScore = document.querySelector('#player-score')
+
+let displayComputerScore = document.querySelector('#computer-score')
+
 console.log(displayResult)
 
 let playerPick
 let min
 let max
-let playerScore
-let computerScore
+let playerScore = 0
+let computerScore = 0
 
 playerPickRock.addEventListener('click', () =>{
     playerPick = 'rock'
     min = 0
     max = 2
-    console.log(round(min, max, playerPick))
-    
+    round(min, max, playerPick, playerScore, computerScore)
+
+    displayPlayerScore.innerText = playerScore
+    displayComputerScore.innerText = computerScore
+
+    winnner(playerScore, computerScore)
 
 })
 
@@ -27,17 +35,42 @@ playerPickPaper.addEventListener('click', () =>{
     playerPick = 'paper'
     min = 0
     max = 2
+    round(min, max, playerPick, playerScore, computerScore)
+
+    displayPlayerScore.innerText = playerScore
+    displayComputerScore.innerText = computerScore
     
+    winnner(playerScore, computerScore)
+
 })
 
 playerPickScissors.addEventListener('click', () =>{
     playerPick = 'scissors'
     min = 0
     max = 2
-    
+    round(min, max, playerPick, playerScore, computerScore)
+
+    displayPlayerScore.innerText = playerScore
+    displayComputerScore.innerText = computerScore
+
+    winnner(playerScore, computerScore)
     
 })
 
+
+function winnner(playerScore, computerScore){
+    if(playerScore == 5){
+        displayResult.innerText = 'You win!!'
+        setTimeout(function () { location.reload(true); }, 5000)
+        return 
+    }else if(computerScore == 5){
+        displayResult.innerText = 'You lost!!'
+        setTimeout(function () { location.reload(true); }, 5000)
+        return 
+    }
+    
+    
+}
 
 
 min = 0
@@ -61,22 +94,22 @@ function round(min, max, playerPick){
     if(computerPick === playerPick){
         return displayResult.innerText = 'Draw'
     }else if(computerPick == 'rock' && playerPick == 'paper'){
-        //playerScore += 1
+        playerScore += 1
         return displayResult.innerText = 'You win! Paper beats rock'
     }else if(computerPick == 'paper' && playerPick == 'scissors'){
-        //playerScore += 1
+        playerScore += 1
         return displayResult.innerText = 'You win! Scissors beat paper'
     }else if(computerPick == 'scissors' && playerPick == 'rock'){
-        //playerScore += 1
+        playerScore += 1
         return displayResult.innerText = 'You win! Rock beats paper'
     }else if(computerPick == 'rock' && playerPick == 'scissors'){
-        //computerScore += 1
+        computerScore += 1
         return displayResult.innerText = 'You lost! Scissors beat rock'
     }else if(computerPick == 'scissors' && playerPick == 'paper'){
-        //computerScore += 1
+        computerScore += 1
         return displayResult.innerText = 'You lost! Scissors beat paper'
     }else if(computerPick == 'paper' && playerPick == 'rock'){
-        //computerScore += 1
+        computerScore += 1
         return displayResult.innerText = 'You lost! Paper beats rock'
     }
 }
